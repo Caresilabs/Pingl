4822fb9b106aabe12fc503ca335cd78ed1f715a6
Simon Bothén <simon.bothen@netset.com>
2018-11-29 14:32:26 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # Pingl
 A Pingdom to Google Chat Relay Bot using Webhooks.
+
+## Instructions
+
+1. Set up a "webhook"-bot in your Google Chat room.
+2. Copy app.config.template to app.config
+2a. Modify the values to your liking
+3. Run `npm install`
+4. Run the node app with: `./index.js`.
+
+## Features
+
+### Pingdom
+Set the pingdom webhook to `YOURDOMAIN.com/pingl?token=TOKEN`.
+
+### Echo
+The bot can echo anything you say by doing a post call to `/echo`. The body should be:
+`{ text: "Hello world!" }`
+
+### Custom messages
+
+Custom messages can be set up in app.config.
+
+Supported replacement tokens are:
+
+{name} - My website
+{hostname} - www.example.com
+{type} - HTTP
+{state} - DOWN
+
---
+++ index.js
@@ -0,0 +1,108 @@
+#!/usr/bin/env node
+
+'use strict';
+
+// ======================================== //
+// Author: Simon Bothén
+// Prototype for Pingdom -> Google Chat
+// ======================================== //
+
+const fs = require('fs');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express().use(bodyParser.json());
+const logger = require('express-logger');
+const format = require('string-format')
+const request = require('request');
+
+const OK_CODE = 200;
+const UNAUTHORIZED_CODE = 401;
+
+let config = JSON.parse(fs.readFileSync('app.config'));
+
+app.use(logger({path: "logs/applog.txt"}));
+
+var server = app.listen(config.port, () => {
+    console.log('[Pingl] is listening!');
+    sendToGoogleChat(getRandomArrayItem(config.messages.botStart), null);
+});
+
+app.post('/pingl', (req, res) => {
+
+    let isAuthorized = authorize(req.query.token);
+    if (isAuthorized != true) {
+        return res.sendStatus(UNAUTHORIZED_CODE);
+    }
+
+    sendPingdomMessage(req.body);
+
+    return res.sendStatus(OK_CODE);
+});
+
+app.post('/echo', (req, res) => {
+
+    let isAuthorized = authorize(req.query.token);
+    if (!isAuthorized) {
+        return res.sendStatus(UNAUTHORIZED_CODE);
+    }
+
+    sendToGoogleChat(req.body.text, null);
+
+    return res.sendStatus(OK_CODE);
+});
+
+function sendPingdomMessage(pingdomData) {
+    let formatData = {
+            name: pingdomData.check_name,
+            state: pingdomData.current_state,
+            type: pingdomData.check_type,
+            hostname: pingdomData.check_params.hostname
+    };
+
+    let template = getRandomArrayItem(getChatTemplatesFromState(pingdomData.current_state));
+    let chatMessage = format(template, formatData);
+
+    sendToGoogleChat(chatMessage,
+        function (error, response, body) {
+            if (!error && response.statusCode == OK_CODE) {
+                console.log(body);
+            }
+        }
+    );
+}
+
+function getChatTemplatesFromState(state) {
+    if (state == "UP") {
+        return config.messages.up;
+    } else {
+        return config.messages.down;
+    }
+}
+
+function sendToGoogleChat(text, callback) {
+    if (text != null && text != "") {
+        request.post(config.googleChatUrl, {json: {text: text}}, callback);
+    }
+}
+
+function authorize(token) {
+    // check if verification token is correct
+    return token == config.token;
+}
+
+function getRandomArrayItem(array) {
+    return array[Math.floor(Math.random()*array.length)];
+}
+
+function onShutdown() {
+    sendToGoogleChat(getRandomArrayItem(config.messages.botShutdown), function () {
+        process.exit();
+    });
+};
+
+// listen for TERM signal .e.g. kill
+process.on('SIGTERM', onShutdown);
+
+// listen for INT signal e.g. Ctrl-C
+process.on('SIGINT', onShutdown);
+
